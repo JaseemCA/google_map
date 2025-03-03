531c7d2e6fcf7df14ee8e89bcb4f725c5799d098
JaseemCA <147582494+JaseemCA@users.noreply.github.com>
2025-03-03 13:44:39 +0530

    ready

---
+++ lib/const.dart
@@ -0,0 +1,3 @@
+//
+
+const String GOOGLE_MAPS_API_KEY = "AIzaSyAV7PnHITGwAW0JUs-xb_q9QRy271-pntQ";
--- lib/mapPage.dart
+++ lib/mapPage.dart
@@ -1,7 +1,183 @@
-import 'dart:async';
-import 'dart:developer';
+// // ignore_for_file: no_leading_underscores_for_local_identifiers
 
+// import 'dart:async';
+// // import 'dart:developer';
+// import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_maps/const.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:location/location.dart';
+
+// class Mappage extends StatefulWidget {
+//   const Mappage({super.key});
+
+//   @override
+//   State<Mappage> createState() => _MappageState();
+// }
+
+// class _MappageState extends State<Mappage> {
+//   final Completer<GoogleMapController> _mapcontroller =
+//       Completer<GoogleMapController>();
+//   Location _locationconntroller = Location();
+//   static const LatLng pCalicut = LatLng(11.2588, 75.7804);
+
+//   static const LatLng pMarineDriveKochi = LatLng(9.9816, 76.2810);
+//   LatLng? _currentP = null;
+//   Map<PolylineId, Polyline> polylines = {};
+
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     getlocationupdates().then((_) => {
+//           getPloyLinepoints().then((coordinates) => {
+//                 generatePolyLineFromPoints(coordinates),
+//                 // print(coordinates),
+//               })
+//         });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _currentP == null
+//           ? const Center(
+//               child: Text("loading..."),
+//             )
+//           : GoogleMap(
+//               onMapCreated: ((controller) =>
+//                   _mapcontroller.complete(controller)),
+//               mapType: MapType.hybrid,
+//               initialCameraPosition: CameraPosition(
+//                 target: pCalicut,
+//                 zoom: 13,
+//               ),
+//               markers: {
+//                 Marker(
+//                     markerId: MarkerId("_currentLocattion"),
+//                     icon: BitmapDescriptor.defaultMarker,
+//                     position: _currentP!),
+//                 Marker(
+//                     markerId: MarkerId("_sourceLocation"),
+//                     icon: BitmapDescriptor.defaultMarker,
+//                     position: pCalicut),
+//                 Marker(
+//                     markerId: MarkerId("_destinationLocation"),
+//                     icon: BitmapDescriptor.defaultMarker,
+//                     position: pMarineDriveKochi),
+//               },
+//               polylines: Set<Polyline>.of(polylines.values),
+//             ),
+//     );
+//   }
+
+// //focusonCurrentLocation
+//   Future<void> _cameraToposition(LatLng pos) async {
+//     final GoogleMapController controller = await _mapcontroller.future;
+//     CameraPosition _newCameraPosition = CameraPosition(
+//       target: pos,
+//       zoom: 13,
+//     );
+//     await controller.animateCamera(
+//       CameraUpdate.newCameraPosition(_newCameraPosition),
+//     );
+//   }
+
+//   //userLocation
+//   Future<void> getlocationupdates() async {
+//     bool _serviceEnabled;
+//     PermissionStatus _permisionGranded;
+
+//     _serviceEnabled = await _locationconntroller.serviceEnabled();
+//     if (_serviceEnabled) {
+//       _serviceEnabled = await _locationconntroller.requestService();
+//     } else {
+//       return;
+//     }
+
+//     _permisionGranded = await _locationconntroller.hasPermission();
+//     if (_permisionGranded == PermissionStatus.denied) {
+//       _permisionGranded = await _locationconntroller.requestPermission();
+//       if (_permisionGranded != PermissionStatus.granted) {
+//         return;
+//       }
+//     }
+
+//     _locationconntroller.onLocationChanged
+//         .listen((LocationData _currentLocation) {
+//       if (_currentLocation.latitude != null &&
+//           _currentLocation.longitude != null) {
+//         setState(() {
+//           _currentP =
+//               LatLng(_currentLocation.latitude!, _currentLocation.longitude!);
+
+//           _cameraToposition(_currentP!);
+//         });
+//       }
+//     });
+//   }
+
+//   Future<List<LatLng>> getPloyLinepoints() async {
+//     List<LatLng> polyLineCordinates = [];
+//     PolylinePoints polylinePoints = PolylinePoints();
+//     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+//         googleApiKey: GOOGLE_MAPS_API_KEY,
+//         request: PolylineRequest(
+//           origin: PointLatLng(pCalicut.latitude, pCalicut.longitude),
+//           destination: PointLatLng(
+//               pMarineDriveKochi.latitude, pMarineDriveKochi.longitude),
+//           mode: TravelMode.driving,
+//         ));
+
+//     if (result.points.isEmpty) {
+//       for (var point in result.points) {
+//         polyLineCordinates.add(LatLng(point.latitude, point.longitude));
+//       }
+//     } else {
+//       // print(result.errorMessage);
+//     }
+//     return polyLineCordinates;
+//   }
+
+//   void generatePolyLineFromPoints(List<LatLng> polycoordinates) {
+//   if (polycoordinates.isEmpty) {
+//     print("No polyline points generated!");
+//     return;
+//   }
+
+//   PolylineId id = PolylineId("poly");
+//   Polyline polyline = Polyline(
+//     polylineId: id,
+//     color: Colors.blue,
+//     points: polycoordinates,
+//     width: 5,
+//     startCap: Cap.roundCap,
+//     endCap: Cap.roundCap,
+//     jointType: JointType.round,
+//   );
+
+//   setState(() {
+//     polylines[id] = polyline;
+//   });
+
+//   print("Polyline added successfully!");
+// }
+
+//   // void generatePolyLineFromPoints(List<LatLng> polycoordinates) async {
+//   //   PolylineId id = PolylineId("poly");
+//   //   Polyline polyline = Polyline(
+//   //       polylineId: id, color: Colors.blue, points: polycoordinates, width: 8);
+//   //   setState(() {
+//   //     polylines[id] = polyline;
+//   //   });
+//   // }
+// }
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
+import 'dart:async';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps/const.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
@@ -13,19 +189,22 @@ class Mappage extends StatefulWidget {
 }
 
 class _MappageState extends State<Mappage> {
-  final Completer<GoogleMapController> _mapcontroller =
+  final Completer<GoogleMapController> _mapController =
       Completer<GoogleMapController>();
-  Location _locationconntroller = Location();
+  Location _locationController = Location();
   static const LatLng pCalicut = LatLng(11.2588, 75.7804);
-
   static const LatLng pMarineDriveKochi = LatLng(9.9816, 76.2810);
-  LatLng? _currentP = null;
+  LatLng? _currentP;
+  Map<PolylineId, Polyline> polylines = {};
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    getlocationupdates();
+    getLocationUpdates().then((_) {
+      getPolyLinePoints().then((coordinates) {
+        generatePolyLineFromPoints(coordinates);
+      });
+    });
   }
 
   @override
@@ -33,76 +212,124 @@ class _MappageState extends State<Mappage> {
     return Scaffold(
       body: _currentP == null
           ? const Center(
-              child: Text("loading..."),
+              child: Text("Loading..."),
             )
           : GoogleMap(
-              onMapCreated: ((controller) =>
-                  _mapcontroller.complete(controller)),
+              onMapCreated: (controller) => _mapController.complete(controller),
               mapType: MapType.hybrid,
-              initialCameraPosition: CameraPosition(
+              initialCameraPosition: const CameraPosition(
                 target: pCalicut,
                 zoom: 13,
               ),
               markers: {
-                  Marker(
-                      markerId: MarkerId("_currentLocattion"),
-                      icon: BitmapDescriptor.defaultMarker,
-                      position: _currentP!),
-                  Marker(
-                      markerId: MarkerId("_sourceLocation"),
-                      icon: BitmapDescriptor.defaultMarker,
-                      position: pCalicut),
-                  Marker(
-                      markerId: MarkerId("_destinationLocation"),
-                      icon: BitmapDescriptor.defaultMarker,
-                      position: pMarineDriveKochi), 
-                }),
+                Marker(
+                    markerId: const MarkerId("_currentLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _currentP!),
+                const Marker(
+                    markerId: MarkerId("_sourceLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: pCalicut),
+                const Marker(
+                    markerId: MarkerId("_destinationLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: pMarineDriveKochi),
+              },
+              polylines: Set<Polyline>.of(polylines.values),
+            ),
     );
   }
 
-//focusonCurrentLocation
-  Future<void> _cameraToposition(LatLng pos) async {
-    final GoogleMapController controller = await _mapcontroller.future;
+  // Focus camera on current location
+  Future<void> _cameraToPosition(LatLng pos) async {
+    final GoogleMapController controller = await _mapController.future;
     CameraPosition _newCameraPosition = CameraPosition(
       target: pos,
-      zoom: 13,
+      zoom: 10,
     );
     await controller.animateCamera(
       CameraUpdate.newCameraPosition(_newCameraPosition),
     );
   }
 
-  //userLocation
-  Future<void> getlocationupdates() async {
+  // Get user location updates
+  Future<void> getLocationUpdates() async {
     bool _serviceEnabled;
-    PermissionStatus _permisionGranded;
+    PermissionStatus _permissionGranted;
 
-    _serviceEnabled = await _locationconntroller.serviceEnabled();
-    if (_serviceEnabled) {
-      _serviceEnabled = await _locationconntroller.requestService();
-    } else {
-      return;
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+      if (!_serviceEnabled) return;
     }
 
-    _permisionGranded = await _locationconntroller.hasPermission();
-    if (_permisionGranded == PermissionStatus.denied) {
-      _permisionGranded = await _locationconntroller.requestPermission();
-      if (_permisionGranded != PermissionStatus.granted) {
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
         return;
       }
     }
 
-    _locationconntroller.onLocationChanged
+    _locationController.onLocationChanged
         .listen((LocationData _currentLocation) {
       if (_currentLocation.latitude != null &&
           _currentLocation.longitude != null) {
         setState(() {
           _currentP =
               LatLng(_currentLocation.latitude!, _currentLocation.longitude!);
-
-          _cameraToposition(_currentP!);
+          _cameraToPosition(_currentP!);
         });
       }
     });
+  }
+
+  // Get polyline points between two locations
+  Future<List<LatLng>> getPolyLinePoints() async {
+    List<LatLng> polyLineCoordinates = [];
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleApiKey: GOOGLE_MAPS_API_KEY,
+      request: PolylineRequest(
+        origin: PointLatLng(pCalicut.latitude, pCalicut.longitude),
+        destination: PointLatLng(
+            pMarineDriveKochi.latitude, pMarineDriveKochi.longitude),
+        mode: TravelMode.driving,
+      ),
+    );
+
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polyLineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    } else {
+      // print("Error fetching polyline: ${result.errorMessage}");
+    }
+    return polyLineCoordinates;
+  }
+
+  // Generate and add polyline to the map
+  void generatePolyLineFromPoints(List<LatLng> polyCoordinates) {
+    if (polyCoordinates.isEmpty) {
+      // print("No polyline points generated!");
+      return;
+    }
+
+    PolylineId id = const PolylineId("poly");
+    Polyline polyline = Polyline(
+      polylineId: id,
+      color: Colors.blue,
+      points: polyCoordinates,
+      width: 5,
+      startCap: Cap.roundCap,
+      endCap: Cap.roundCap,
+      jointType: JointType.round,
+    );
+
+    setState(() {
+      polylines[id] = polyline;
+    });
+
+    // print("Polyline added successfully!");
   }
 }
